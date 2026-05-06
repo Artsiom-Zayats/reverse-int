43e957c8aa8cb7978247ae908c64a46767a655a0
Artsiom Zayats <ctapbiu.zayats@gmail.com>
2026-05-06 16:27:06 +0300

    fix: update function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function reverse(n) {
   let number = n;
-  if (n<0){
-    number = n*(-1);
+  if (n < 0) {
+    number = n * -1;
   }
- 
-  return parseInt(String(number).split('').reverse().join(''));
-  };
+
+  return parseInt(String(number).split('').reverse().join(''), 10);
+};
